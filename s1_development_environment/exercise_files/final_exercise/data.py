bfa4bdab9e142afc736f4225280bce6d9022498c
Katk2 <58248346+Katk2@users.noreply.github.com>
2024-01-03 11:06:25 +0100

    Update data.py

--- s1_development_environment/exercise_files/final_exercise/data.py
+++ s1_development_environment/exercise_files/final_exercise/data.py
@@ -1,23 +1,37 @@
 import torch
 import numpy as np
 from torchvision import datasets, transforms
-
-
-
+import os
+from torchvision.io import read_image
+from torch.utils.data import Dataset, DataLoader
 
 
 def mnist():
     """Return train and test dataloaders for MNIST."""
     # exchange with the corrupted mnist dataset
-    datapath = torch.load('data/corrupted_mnist.pt')
-    transform = transforms.Compose([transforms.ToTensor(),
-                              transforms.Normalize((0.5,), (0.5,)),
-                              ])
-    trainset = datasets.MNIST(datapath, download=True, train=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+    BASEDIR = "C:\programmering\DTU\mlops\data\corruptmnist"
+    imgTrain = "train_images_0.pt"
+    labelTrain = "train_target_0.pt"
+    imgTest = "test_images.pt"
+    labelTest = "test_target.pt"
+
+    imgPathTrain = os.path.join(BASEDIR, imgTrain) 
+    labelPathTrain = os.path.join(BASEDIR, labelTrain)
+    imgPathTest = os.path.join(BASEDIR, imgTest)
+    labelPathTest = os.path.join(BASEDIR, labelTest)
+
+    dataTrain = torch.load(imgPathTrain).view(-1, 1, 28, 28)
+    labelsTrain = torch.load(labelPathTrain)
+    dataTest = torch.load(imgPathTest).view(-1, 1, 28, 28)
+    labelsTest = torch.load(labelPathTest)
     
-    
-    
-    train = torch.randn(50000, 784)
-    test = torch.randn(10000, 784)
+
+    trainDataset = [[x, y] for x, y in zip(dataTrain, labelsTrain)]
+    testDataset = [[x, y] for x, y in zip(dataTest, labelsTest)]
+
+
+    train = DataLoader(trainDataset, batch_size=64, shuffle=True)
+    test = DataLoader(testDataset, batch_size=64, shuffle=True)
+
+
     return train, test
